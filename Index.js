2f57f2177a3151f2e1d7d5c45260e79ca2b2c7ab
Santi Arnau <santi07a@gmail.com>
2021-03-31 13:59:15 +0200

    agregada api a .env

--- Index.js
+++ Index.js
@@ -1,8 +1,13 @@
 const { program } = require("commander");
 const inquirer = require("inquirer");
+const dotenv = require("dotenv").config();
 const fetch = require("node-fetch");
 const chalk = require("chalk");
 const preguntas = require("./utilidades/Preguntas");
+
+const datos = fetch(process.env.TMB_API_LINEAS)
+  .then(resp => resp.json())
+  .then(datos => datos);
 
 let mensaje;
 
@@ -13,6 +18,12 @@ inquirer.prompt(preguntas).then(respuestas => {
   }
   if (respuestas.errores === true) {
     mensaje = chalk.red.bold("La línea no existe");
+  }
+
+  if (respuestas.linea === datos.features.properties.NOM_LINIA) {
+    if (color) {
+      console.log(chalk.hex(color)(`Línea ${respuestas.linea}.`));
+    }
   }
 });
 
